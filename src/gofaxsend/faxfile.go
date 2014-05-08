dc7c2316f928f8a3ed6dc3058a9d6a140a211b45
Markus Lindenberg <lindenberg@gonicus.de>
2014-05-08 18:17:31 +0200

    Add GPL 2.0 License and License Headers

--- src/gofaxsend/faxfile.go
+++ src/gofaxsend/faxfile.go
@@ -1,3 +1,20 @@
+// This file is part of the GOfax.IP project - https://github.com/gonicus/gofaxip
+// Copyright (C) 2014 GONICUS GmbH, Germany - http://www.gonicus.de
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; version 2
+// of the License.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
 package main
 
 import (
