5de57a74306c57f297904ef4b8329ef95b547143
Sebastian Denz <denzs@gonicus.de>
2020-09-07 13:29:00 +0200

    Update docker build container from stretch to buster

--- packaging/generate_packages.sh
+++ packaging/generate_packages.sh
@@ -7,6 +7,6 @@ else
     version="master"
 fi
 
-docker pull debian:stretch
+docker pull debian:buster
 docker build -t gofaxip_build .
 docker run --rm -v $PWD/..:/input -v $PWD:/output -e VERSION=$version gofaxip_build
